6a623886e32bc6aeecbcbdd439ff4798b042a1a4
TOGOLAIS DU MONDE ENTIER <54533757+TGME@users.noreply.github.com>
2019-10-17 13:03:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -70,3 +70,13 @@ changeWord();
 setInterval(changeWord, 4000);
 
 });
+
+
+$(document).ready(function(){
+  $('.img-zoom').hover(function() {
+  $(this).addClass('transition');
+ 
+  }, function() {
+    $(this).removeClass('transition');
+  });
+});
